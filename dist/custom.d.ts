ea861f2c72da236669ab45dea532b55ffb51aa3c
Souvik Roy Chowdhury <57339549+souvikelric@users.noreply.github.com>
2025-03-07 21:50:10 +0530

    Delete dist directory

--- dist/custom.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const runCommand: (command: string) => Promise<string>;
-declare function getOutput(): Promise<string[]>;
-export declare function createReactProject(projectName: string, isTypeScript: boolean): Promise<string>;
-export default getOutput;
